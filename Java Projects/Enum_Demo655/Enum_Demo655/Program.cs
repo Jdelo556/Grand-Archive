8f3b26fa5700778c3d107dd61bf93378c410cc8c
Joseph Delorenzo <josephdelorenzo@Josephs-MacBook-Pro-5.local>
2022-06-15 18:38:46 -0500

    Update2

---
+++ Java Projects/Enum_Demo655/Enum_Demo655/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Example556
+{
+    internal static class example556
+    {
+        static void Main(string[] args)
+        {
+            String name4 = gemStones.roseQuartz.ToString();
+            String name1 = gemStones.Quartz.ToString();
+            String name6 = gemStones.Gold.ToString();
+            String name9 = gemStones.Jade.ToString();
+
+            Console.WriteLine("This is one of my favorite gems," + name4 + ",it is great for loving and nurturing yourself and others");
+            Console.WriteLine(name1 + " is for amplifying the current energy that is around " );
+            Console.WriteLine("This is one of the most versitle metals on Earth, " + name6 + " is the quintesential symbol of wealth, as well as the best electrical conductor");
+            Console.WriteLine("Ita uh shimmers rike a most precious " + name9);
+
+
+
+        }
+        enum gemStones
+        {
+            Quartz = 1,
+            Amathyst = 2,
+            Agate = 3,
+            roseQuartz = 4,
+            Pyrites = 5,
+            Gold = 6,
+            Silver = 7,
+            Urainium = 8,
+            Jade  =  9,
+        }
+
+    }
+
+}
+
+
---
+++ c# and .NET/BankingLIB/BankingLIB/Checking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankingLIB;
+
+namespace BankingLIB
+{
+	public class Checking : Accounts
+	{
+		public Checking()
+		{
+		}
+	}
+}
+
